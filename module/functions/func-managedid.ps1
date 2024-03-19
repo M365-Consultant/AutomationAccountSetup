f9b08b1ab84586777f53dceb4b253131a93b6807
M365-Consultant <dominik@dg365.de>
2024-03-19 14:09:47 +0100

    V0.0.3

--- module/functions/func-managedid.ps1
+++ module/functions/func-managedid.ps1
@@ -24,14 +24,14 @@ b ...back to main menu
 Select"){
         s {az_automation_set -breadcrumb M365cdeMIDgraph}
         m {managedid_define}
-        1 {funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope 'User.Read.All'}
-        2 {funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope 'User.ReadWrite.All'}
-        3 {funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope 'Group.Read.All'}
-        4 {funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope 'Group.ReadWrite.All'}
-        5 {funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope 'UserAuthenticationMethod.Read.All'}
-        6 {funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope 'AuditLog.Read.All'}
-        7 {funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope 'Policy.ReadWrite.ConditionalAccess'}
-        8 {funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope 'Mail.Send'}
+        1 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope 'User.Read.All'}
+        2 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope 'User.ReadWrite.All'}
+        3 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope 'Group.Read.All'}
+        4 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope 'Group.ReadWrite.All'}
+        5 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope 'UserAuthenticationMethod.Read.All'}
+        6 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope 'AuditLog.Read.All'}
+        7 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope 'Policy.ReadWrite.ConditionalAccess'}
+        8 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope 'Mail.Send'}
         c {managedid_custom}
         e {M365cdeMIDexo}
         b {M365cdeAAASetup}
@@ -39,28 +39,81 @@ Select"){
     }
 }
 
-#Function for Graph Scope assignment
-function funcGraphScopeAssignment() {
+function M365cdeMIDexo(){
+    Clear-Host
+    switch(Read-Host "Please select an option `
+--------------------------------------------------
+1 Add Scope 'Exchange.ManageAsApp'
+2 Add Scope 'MailboxSettings.Read'
+3 Add Scope 'MailboxSettings.ReadWrite'
+4 Add Scope 'Mail.Read'
+5 Add Scope 'Mail.ReadBasic'
+6 Add Scope 'Mail.ReadWrite'
+7 Add Scope 'Mail.Send'
+8 Add Scope 'Calenders.Read'
+9 Add Scope 'Calenders.ReadWrite'
+10 Add Scope 'Contacts.Read'
+11 Add Scope 'Contacts.ReadWrite'
+c Add custom Exchange Scope
+--------------------------------------------------
+r1 Add Role 'Exchange Recipient Administrator'
+r2 Add Role 'Exchange Administrator'
+rc Add custom Exchange Role
+--------------------------------------------------
+
+b ...back to previous menu
+
+Select"){
+        1 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Exchange.ManageAsApp'}
+        2 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'MailboxSettings.Read'}
+        3 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'MailboxSettings.ReadWrite'}
+        4 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Mail.Read'}
+        5 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Mail.ReadBasic'}
+        6 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Mail.ReadWrite'}
+        7 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Mail.Send'}
+        8 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Calenders.Read'}
+        9 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Calenders.ReadWrite'}
+        10 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Contacts.Read'}
+        11 {funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope 'Contacts.ReadWrite'}
+        c  {managedid_customexchangescope}
+        r1 {funcEntraRoleAssignment -ManagedIdentityID $AutomationAccountMId -EntraRole 'Exchange Recipient Administrator'}
+        r2 {funcEntraRoleAssignment -ManagedIdentityID $AutomationAccountMId -EntraRole 'Exchange Administrator'}
+        rc {managedid_customexchangerole}
+        b {M365cdeMIDgraph}
+        default {M365cdeMIDexo}
+    }
+}
+
+#Function for Scope assignment
+function funcScopeAssignment() {
     param (
         $ManagedIdentityID,
-        $GraphScope
+        $Scope,
+        $target
     )
 
+    $appIds = @{
+        "graph" = '00000003-0000-0000-c000-000000000000'
+        "exo"   = '00000002-0000-0ff1-ce00-000000000000'
+    }
+
+    $appId = $appIds[$target]
+
     If($AutomationAccountMId) {
-        $AppGraph = Get-MgServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
-        $AppRole = $AppGraph.AppRoles | Where-Object {$_.Value -eq $GraphScope}
+        $AppGraph = Get-MgServicePrincipal -Filter "AppId eq '$appId'"
+        $AppRole = $AppGraph.AppRoles | Where-Object {$_.Value -eq $Scope}
 
         if ($appRole) {
                 $existingAppRole = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $ManagedIdentityID | Where-Object { $_.ResourceId -eq $AppGraph.Id -and $_.AppRoleId -eq $AppRole.Id }
-                if ($existingAppRole) { Write-Warning "The scope '$GraphScope' is already assigned" }
+                if ($existingAppRole) { Write-Warning "The scope '$Scope' is already assigned" }
                 else{
                     New-MgServicePrincipalAppRoleAssignment -PrincipalId $ManagedIdentityID -ServicePrincipalId $ManagedIdentityID -ResourceId $AppGraph.Id -AppRoleId $AppRole.Id > $null
                     $existingAppRole = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $ManagedIdentityID | Where-Object { $_.ResourceId -eq $AppGraph.Id -and $_.AppRoleId -eq $AppRole.Id }
-                        if ($existingAppRole) { Write-Output "The scope '$GraphScope' has been assigned" }
-                        else { Write-Warning "The scope '$GraphScope' could not be assigned" }
+                        if ($existingAppRole) { Write-Output "The scope '$Scope' has been assigned" }
+                        else { Write-Warning "The scope '$Scope' could not be assigned" }
                 }
             }
-        else { Write-Warning "No App Role found for scope '$GraphScope'"}
+        else { Write-Warning "No App Role found for scope '$Scope'"}
     }
     else {
         Write-Warning "`nManaged Identity Object ID is not defined!`n`nDefine it via the options s (from a existing Automation Account) or m (manually) and try it again!"
@@ -68,9 +121,11 @@ function funcGraphScopeAssignment() {
         M365cdeMIDgraph
     }
     Start-Sleep -Seconds 3
-    M365cdeMIDgraph
+    if ($target -eq "graph") {M365cdeMIDgraph}
+    elseif ($target -eq "exo") {M365cdeMIDexo}
 }
 
+#Function for Managed Identity Object ID definition
 function managedid_define(){
             $AutomationAccountMId = Read-Host "Enter the Object ID of your Managed Identity"
             Write-Output "Managed Identity ID is set to: $AutomationAccountMId"
@@ -79,68 +134,16 @@ Press Enter to continue…')
             M365cdeMIDgraph
 }
 
+#Function for Custom Scope assignment
 function managedid_custom(){
         $ScopeCustom = Read-Host "Enter the Scope-Name (e.g. 'User.Read.All')"
-        funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope $ScopeCustom
+        funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'graph' -Scope $ScopeCustom
             (Read-Host '
 Press Enter to continue…')
             M365cdeMIDgraph
 }
 
-
-function M365cdeMIDexo(){
-    Clear-Host
-    switch(Read-Host "Please select an option `
---------------------------------------------------
-1 Add Scope 'Exchange.ManageAsApp'
-2 Add Scope 'MailboxSettings.ReadWrite'
-cs Add custom Exchange Scope
---------------------------------------------------
-3 Add Role 'Exchange Recipient Administrator'
-4 Add Role 'Exchange Administrator'
-cr Add custom Exchange Role
---------------------------------------------------
-
-b ...back to previous menu
-
-Select"){
-        1 {funcExchangeScopeAssignment -ManagedIdentityID $AutomationAccountMId -ExchangeScope 'Exchange.ManageAsApp'}
-        2 {funcExchangeScopeAssignment -ManagedIdentityID $AutomationAccountMId -ExchangeScope 'MailboxSettings.ReadWrite'}
-        3 {funcEntraRoleAssignment -ManagedIdentityID $AutomationAccountMId -EntraRole 'Exchange Recipient Administrator'}
-        4 {funcEntraRoleAssignment -ManagedIdentityID $AutomationAccountMId -EntraRole 'Exchange Administrator'}
-        cs {managedid_customexchangescope}
-        cr {managedid_customexchangerole}
-        b {M365cdeMIDgraph}
-        default {M365cdeMIDexo}
-    }
-}
-
-function funcExchangeScopeAssignment() {
-    param (
-        $ManagedIdentityID,
-        $ExchangeScope
-    )
-
-    If($AutomationAccountMId) {
-        $AppGraph = Get-MgServicePrincipal -Filter "AppId eq '00000002-0000-0ff1-ce00-000000000000'"
-        $AppRole = $AppGraph.AppRoles | Where-Object {$_.Value -eq $ExchangeScope}
-
-        if ($appRole) {
-                $existingAppRole = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $ManagedIdentityID | Where-Object { $_.ResourceId -eq $AppGraph.Id -and $_.AppRoleId -eq $AppRole.Id }
-                if ($existingAppRole) { Write-Warning "The scope '$ExchangeScope' is already assigned" }
-                else{New-MgServicePrincipalAppRoleAssignment -PrincipalId $ManagedIdentityID -ServicePrincipalId $ManagedIdentityID -ResourceId $AppGraph.Id -AppRoleId $AppRole.Id ; Write-Output "The scope '$ExchangeScope' has been assigned" }
-            }
-        else { Write-Warning "No App Role found for scope '$ExchangeScope'"}
-    }
-    else {
-        Write-Warning "`nManaged Identity Object ID is not defined!`n`nDefine it via the options s (from a existing Automation Account) or m (manually) and try it again!"
-        Start-Sleep -Seconds 5
-        M365cdeMIDgraph
-    }
-    Start-Sleep -Seconds 3
-    M365cdeMIDexo
-}
-
+#Function for Role assignment
 function funcEntraRoleAssignment() {
     param (
         $ManagedIdentityID,
@@ -167,13 +170,15 @@ function funcEntraRoleAssignment() {
     M365cdeMIDexo
 }
 
+#Function for Custom Role assignment EXO
 function managedid_customexchangescope(){
     $ExchangeScopeCustom = Read-Host "Enter the Scope-Name (e.g. 'MailboxSettings.ReadWrite')"
-    funcGraphScopeAssignment -ManagedIdentityID $AutomationAccountMId -GraphScope $ExchangeScopeCustom
+    funcScopeAssignment -ManagedIdentityID $AutomationAccountMId -target 'exo' -Scope $ExchangeScopeCustom
     Start-Sleep -Seconds 3
     M365cdeMIDexo
 }
 
+#Function for Custom Role assignment EXO
 function managedid_customexchangerole(){
     $ExchangeRoleCustom = Read-Host "Enter the Role-Name (e.g. 'Exchange Recipient Administrator')"
     funcEntraRoleAssignment -ManagedIdentityID $AutomationAccountMId -EntraRole $ExchangeRoleCustom
